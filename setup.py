f1eb434b1c8e7019ca3209cbbbef8a6934229120
Marc Brinkmann <git@marcbrinkmann.de>
2016-02-03 02:00:15 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    buf = open(os.path.join(os.path.dirname(__file__), fname), 'rb').read()
+    return buf.decode('utf8')
+
+
+setup(
+    name='ticking',
+    version='0.0.1.dev1',
+    description=
+    'A small timing utility library, handling framerates and rough benchmarks.',
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='https://github.com/mbr/ticking',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+    ])
